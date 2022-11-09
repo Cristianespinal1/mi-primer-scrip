a4df039ea7681717a7e3411f167f1d4516740275
Cristianespinal1 <115481942+Cristianespinal1@users.noreply.github.com>
2022-11-09 10:34:40 -0400

    Add files via upload

---
+++ ptrigo.js
@@ -0,0 +1,10 @@
+// tigonometria//
+var x=(Math.PI)/5;
+
+let e=Math.cos(x/2);
+let c=Math.pow(e, 2);
+document.write ("<h4 clase=suma> el valor de cos^2(x) si x= pi/5 es: "+c+ "</h4>")
+var h=(Math.tan(x) + Math.sin(x))/(2*Math.tan(x));
+
+document.write ("<h4 clase=suma> el valor de (tan(x)+sen(x))/(2*tan(x)) si x= pi/5 es: "+h+ "</h4>")
+
